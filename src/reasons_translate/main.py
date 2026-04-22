ab56842f9774c50cc2749fab00db4060a61fa66d
VitaoDeveloper <paulo.borgess.prates@gmail.com>
2026-04-22 18:01:31 -0300

    Executando algoritmo de tradução do reasons.json

--- src/reasons_translate/main.py
+++ src/reasons_translate/main.py
@@ -10,8 +10,9 @@ with open('assets/reasons.json', 'r', encoding='utf-8') as file:
 result = []
 
 for reason in reasons:
-    translate = GoogleTranslator(source='pt', target='en').translate(reason)
-    result.append(reason)
-
-with open('assets/reason_pt.json', 'w', encoding='utf-8') as file:
-    json.dump(result, file, ensure_ascii=False, indent=4)
+    try:
+        translate = GoogleTranslator(source='en', target='pt').translate(reason)
+        result.append(translate)
+    finally:
+        with open('assets/reason_pt.json', 'w', encoding='utf-8') as file:
+            json.dump(result, file, ensure_ascii=False, indent=4)
